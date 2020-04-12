cedb3da6c0fd7e39181ef0c0ee228b1b84c639c1
Ugwuanyi Ekene <love2ekene@gmail.com>
2020-04-12 12:49:49 +0100

    First commit on challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
 
+// const impact = {};
+// const severeImpact = {};
+
+const currentlyInfected = data.reportedCases * 10;
+const svrcurrentlyInfected = data.reportedCases * 50;
+const totalBed = data.totalHospitalBeds;
+cosnt avgIncome = data.region.avgDailyIncomeInUSD;
+const avgIncPop = data.region.avgDailyIncomePopulation;
+
+let days;
+let dollarsInFlight;
+let svrDollarsInFlight;
+if ( data.periodType === "days" ) {
+    days = data.timeToElapse;
+}
+else if ( data.periodType === "weeks" ) {
+    days = data.timeToElapse * 7;
+}
+else if ( data.periodType === "months") {
+    days = data.timeToElapse * 30;
+}
+ 
+}
 export default covid19ImpactEstimator;
